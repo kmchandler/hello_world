a8d6f06fce3bc521efc3e14a74e75c7a77e0541f
Kristen Chandler <krismchandler@gmail.com>
2022-10-06 16:30:36 -0500

    completed exercise

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,5 @@
+import random
+
 animals = [ "Triceratops", "Gorilla", "Corgi", "Toucan"]
 
 greeting = input("Please choose from the following greetings: Input 1 for Southern, 2 for English, and 3 for German")
@@ -14,7 +16,17 @@ while len(greeting) <=1:
       break
   else:
       print("Howdy, Y'all!")
+    
+animal_names = input("List out some funny animal names:")
+animal_list = animal_names.split(' ')
+for num, animal in enumerate(animal_list,1):
+     print(num, animal)
 
 for animal in animals:
     if len(animal) >= 5:
         print(animal)
+
+random_animal = random.choice(animal_list)
+
+fav_color = input("What is your favorite color?")
+print(f"Whould you like to have a {fav_color} {random_animal}")
